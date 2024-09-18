cabbdf17b8ce66ff6f6954902f714c2778ba042d
Arman Mokhtari <rmn.mokhtarii@gmail.com>
2024-09-18 14:00:40 +0330

    back navigation created

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+import styles from "./Button.module.css";
+
+const Button = ({ children, onClick, type }) => {
+  return (
+    <button onClick={onClick} className={`${styles.btn} ${styles[type]}`}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
