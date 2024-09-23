5498f70c0f55e0bc4e762f7cdcf1aafd881b9e12
Arman Mokhtari <rmn.mokhtarii@gmail.com>
2024-09-23 18:27:48 +0330

    logout implemented

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -60,5 +60,6 @@ function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined)
     throw new Error("AuthContext was used outside AuthProvider");
+  return context;
 }
 export { AuthProvider, useAuth };
